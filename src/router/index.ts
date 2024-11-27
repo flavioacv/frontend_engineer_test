1d9239613bc576f8095e031e4ba0c9d622f756de
Flavio Augusto <flavioacv.soft@gmail.com>
2024-11-27 00:44:15 -0400

    arquivo adicionando e configurando

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AddCategory from '../views/AddCategory.vue'
+import Category from '../views/Category.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'Category',
+      component: Category,
+    },
+    {
+      path: '/add-category',
+      name: 'AddCategory',
+      component: AddCategory,
     },
     {
       path: '/about',
