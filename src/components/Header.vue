16cc11825a720057115b7a14e1941e9d56e56d0e
Flavio Augusto <flavioacv.soft@gmail.com>
2024-11-27 00:42:25 -0400

    melhoria no component Header para receber funcao e correcao de espaco

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="d-flex align-items-center justify-content-between py-3 ">
-        <!-- Botão ou ícone na esquerda -->
-        <button v-if="iconBack" class="btn p-0 btn-custom">
-            <i class="bi bi-arrow-left icon-large "></i>
-        </button>
+  <div class="d-flex align-items-center justify-content-between py-3 ">
+    <!-- Botão ou ícone na esquerda -->
+    <button class="btn p-0 btn-custom" :style="{ color: iconBack ? '#DA3468' : '#FFFFFF' }" @click="onClick">
+      <i class="bi bi-arrow-left icon-large "></i>
+    </button>
 
-        <!-- Título centralizado -->
-        <h1 class="fs-5 mb-0 text-center flex-grow-1">{{ title }}</h1>
+    <!-- Título centralizado -->
+    <h1 class="fs-5 mb-0 text-center flex-grow-1">{{ title }}</h1>
 
-
-    </div>
+    <button class="btn p-0" style=" color:#FFFFFF ">
+      <i class="bi bi-arrow-left icon-large "></i>
+    </button>
+  </div>
 </template>
 
 <script setup>
@@ -17,30 +19,36 @@ import { defineProps } from 'vue';
 
 // Recebe o título como propriedade
 defineProps({
-    title: {
-        type: String,
-        required: true,
-    },
-    iconBack: {
-        type: Boolean,
-        required: true,
-        default: false,
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  iconBack: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  onClick: {
+    type: Function,
+    default: null,
+  },
 });
+
+
 </script>
 
 <style scoped>
 .btn-custom {
-    color: #DA3468;
-    /* Texto branco */
-    border: none;
-    /* Sem borda */
+  color: #DA3468;
+  /* Texto branco */
+  border: none;
+  /* Sem borda */
 
 }
 
 /* Ícone com tamanho maior */
 .icon-large {
-    font-size: 1.5rem;
-    /* Aumenta o tamanho do ícone */
+  font-size: 1.5rem;
+  /* Aumenta o tamanho do ícone */
 }
 </style>
