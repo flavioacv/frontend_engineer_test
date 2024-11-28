8b2de0dfd91163c34b5c22142fa45e5eace67667
Flavio Augusto <flavioacv.soft@gmail.com>
2024-11-28 15:27:12 -0400

    Component de menu de categoria e dialog de exclusao de item

---
+++ src/components/CategoryMenuComponent.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="category-menu d-flex align-items-center justify-content-between p-3 mb-2 border">
+    <!-- Nome da categoria -->
+    <div><span class="text-dark" style="font-weight: 500;">{{ categoryName }}</span> <span v-if="categoryCount"
+        class="badge rounded-pill ms-3"
+        style="background-color: #DDE2EF; color: #586893; font-size: 18px; padding: 5px 15px; ">{{ categoryCount
+        }}</span>
+    </div>
+
+
+    <a class="btn btn-sm  me-2  " role="button">
+      <i class="bi bi-chevron-right" style="font-size: 1rem ; color: #8693B4;"></i>
+
+    </a>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+// Propriedades
+defineProps({
+  categoryName: {
+    type: String,
+    required: true,
+  },
+  categoryCount: {
+    type: Number,
+  },
+});
+
+</script>
+
+<style scoped>
+.category-menu {
+  border: 1;
+  border-radius: 20px;
+  border-color: #DDE2EF;
+  /* box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); */
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.category-item:hover {
+  background-color: #ECEEF4;
+}
+</style>
---
+++ src/components/DialogDeleteComponent.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="modal fade" id="ModalDelete" aria-hidden="true" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered" style="border-radius: 20px;">
+      <div class="modal-content">
+
+        <div class="modal-body " style="justify-content: start !important;">
+          <div>
+            <i class="bi bi-exclamation-triangle-fill" style="font-size: 80px; color: #EEBB39;"></i>
+          </div>
+
+          <h2 class="fw-bold fs-5 mb-2" style="color: #29354F;">{{ title }}</h2>
+          <p style="color: #586893;">{{ description }}
+          </p>
+        </div>
+        <div class="modal-footer" style="border: none; justify-content: center;">
+          <button class="btn rounded-pill " data-bs-dismiss="modal" @click="onCancel"
+            style="border: none;padding: 10px 30px; background-color: #FFE2EB; color: #DA3468;">
+            {{ textCancel }}
+          </button>
+          <button class="btn rounded-pill" @click="onConfirm" data-bs-dismiss="modal"
+            style="border: none;padding: 10px 35px; background-color: #F24F82; color: #FFFFFF;">
+            {{ textConfirm }}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+// Importa o tipo correto
+import type { PropType } from "vue";
+
+// Props para controlar ações
+defineProps({
+  onConfirm: {
+    type: Function as PropType<(event: MouseEvent) => void>,
+    required: true, // Função a ser chamada ao clicar em "Excluir"
+  },
+  onCancel: {
+    type: Function as PropType<(event: MouseEvent) => void>,
+    default: () => { }, // Função opcional ao clicar em "Cancelar"
+  },
+  textConfirm: {
+    type: String,
+    default: 'Excluir',
+  },
+  textCancel: {
+    type: String,
+    default: 'Cancelar',
+  },
+  title: {
+    type: String,
+    default: 'Deseja excluir a categoria?',
+  },
+  description: {
+    type: String,
+    default: 'Essa ação é irreversível e implica na exclusão das subcategorias vinculadas. Os produtos ligados a essa categoria não serão excluídos.',
+  },
+
+});
+</script>
+
+
+<style scoped>
+.modal-content {
+  background-color: #fff;
+  border-radius: 20px;
+}
+</style>
