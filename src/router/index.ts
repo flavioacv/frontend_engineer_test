578b47825179e118b91fdb4b2b0c2a5c310edb1b
Flavio Augusto <flavioacv.soft@gmail.com>
2024-11-28 15:56:07 -0400

    navegacao ajustada

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
+import AddSubcategory from '@/views/AddSubcategoryView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
-import AddCategory from '../views/AddCategory.vue'
-import Category from '../views/Category.vue'
+import AddCategory from '../views/AddCategoryView.vue'
+import Category from '../views/CategoryView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,12 +17,9 @@ const router = createRouter({
       component: AddCategory,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/add-subcategory',
+      name: 'AddSubcategory',
+      component: AddSubcategory,
     },
   ],
 })
