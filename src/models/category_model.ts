7ac12bbadd66626c40017fc184685a52a0e9501a
Flavio Augusto <flavioacv.soft@gmail.com>
2024-11-28 15:55:55 -0400

    modelos atualizados

--- src/models/category_model.ts
+++ src/models/category_model.ts
@@ -1,6 +1,21 @@
-import type SubCategoryModel from "./subcategory_model";
+import SubCategoryModel from "./subcategory_model";
 
 export default class CategoryModel {
-	constructor(readonly name: string, readonly id: String, readonly subCategory: SubCategoryModel[]) {
+	constructor(readonly name: string, readonly id: string, public subCategory: SubCategoryModel[]) {
 	}
-}	
+
+    // // Método para criar uma instância de CategoryModel a partir de um objeto JSON
+    // static fromJson(json: any): CategoryModel {
+    //   const subCategory = json.subCategory.map((item: any) => SubCategoryModel.fromJson(item));
+    //   return new CategoryModel(json.name, json.id, subCategory);
+    // }
+
+    // // Método para converter uma instância de CategoryModel para um objeto JSON
+    // toJson(): object {
+    //   return {
+    //     name: this.name,
+    //     id: this.id,
+    //     subCategory: this.subCategory.map((sub) => sub.toJson()), // Assume que SubCategoryModel também tenha o método toJson
+    //   };
+    // }
+}
--- src/models/subcategory_model.ts
+++ src/models/subcategory_model.ts
@@ -1,6 +1,17 @@
 export default class SubCategoryModel {
-    constructor(readonly id: String, readonly name: string) {
+    constructor(readonly id: string, readonly name: string) {
     }
+//  // Método estático para criar uma instância de SubCategoryModel a partir de um objeto JSON
+//  static fromJson(json: any): SubCategoryModel {
+//   return new SubCategoryModel(json.id, json.name);
+// }
 
+// // Método para converter uma instância de SubCategoryModel para um objeto JSON
+// toJson(): object {
+//   return {
+//     id: this.id,
+//     name: this.name
+//   };
+// }
 
 }
