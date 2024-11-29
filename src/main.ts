dbba8f0095d8f333266ccf3a0866ab6b683ef6f4
Flavio Augusto <flavioacv.soft@gmail.com>
2024-11-28 23:49:04 -0400

    teste para usar pinia persist

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,21 @@
-import './assets/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/main.css';
 
-import { createPinia } from 'pinia'
-import { createApp } from 'vue'
+import { createPinia } from 'pinia';
+// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
+import { createApp } from 'vue';
 
-import App from './App.vue'
-import './assets/styles/fonts.css'
-import router from './router'
+import App from './App.vue';
+import './assets/styles/fonts.css';
+import router from './router';
 
 const app = createApp(App)
+const pinia = createPinia();
+// pinia.use(piniaPluginPersistedstate);
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
