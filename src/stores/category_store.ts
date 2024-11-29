9d3293b8c2e334bfd6955f1405b29841a79d3f6d
Flavio Augusto <flavioacv.soft@gmail.com>
2024-11-28 23:53:37 -0400

    ajsute de loading e Erro

--- src/stores/category_store.ts
+++ src/stores/category_store.ts
@@ -3,19 +3,28 @@ import { defineStore } from 'pinia';
 
 
 export const useCategoryStore = defineStore('category', {
-    state: () => ({
-        categories: [] as CategoryModel[],
-        isLoading: false,
-        error: null as string | null,
-    }),
+  state: () => ({
+    categories: [] as CategoryModel[],
+    isLoading: false,
+    error: null as string | null,
+  }),
 
-    actions: {
-        setCategories(categories: CategoryModel[]) {
-            this.categories = categories;
-        },
+  actions: {
+    setCategories(categories: CategoryModel[]) {
+      this.categories = categories;
     },
-
-    getters:{
-        getCategory: (state) => state.categories as  CategoryModel[],
+    setLoading(value: boolean) {
+      this.isLoading = value;
+    },
+    setError(value: string) {
+      this.error = value
     }
+  },
+
+  getters: {
+    getCategory: (state) => state.categories as CategoryModel[],
+    getLoading: (state) => state.isLoading as boolean,
+    getError: (state) => state.error as string,
+  },
+
 });
