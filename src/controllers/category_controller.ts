286f5bd602bbaf749c3824a6b89be4ad7a58c251
Flavio Augusto <flavioacv.soft@gmail.com>
2024-11-28 23:52:29 -0400

    controller ajustado

--- src/controllers/category_controller.ts
+++ src/controllers/category_controller.ts
@@ -1,45 +1,42 @@
 import AppException from "@/exception/app_exception";
-import type CategoryRepository from "@/repositories/category_repository";
+import CategoryRepositoryImpl from "@/repositories/impl/category_repository_impl";
+import HttpServiceImpl from "@/services/http_service/impl/http_service_impl";
 import { useCategoryStore } from "@/stores/category_store";
 
 
 
 export default class CategoryController {
-    private categoryRepository: CategoryRepository;
-    private categoryStore = useCategoryStore();
-    constructor(categoryRepository: CategoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
-
-    // getCategory(refresh: Boolean = false) {
-    //     if (this.$state.categories && this.$state.categories.length > 0 && !refresh) {
-    //         return this.categories;
-    //     }
-
-    // }
-
-    async loadCategories(refresh: Boolean = false): Promise<void> {
-
-        this.categoryStore.isLoading = true;
-        this.categoryStore.error = null;
+  private httpService = new HttpServiceImpl();
+  private categoryRepository = new CategoryRepositoryImpl(this.httpService);
+  private categoryStore = useCategoryStore();
 
 
-        if (this.categoryStore.getCategory.length === 0 && refresh) {
-            try {
-                const categories = await this.categoryRepository.getCategory();
-                this.categoryStore.setCategories(categories);
-            } catch (error) {
-                if (error instanceof AppException) {
-                    this.categoryStore.error = `Failed to load categories: ${error.message}`;
-                } else {
-                    this.categoryStore.error = 'An unexpected error occurred.';
-                }
-                console.error(this.categoryStore.error, error);
-            } finally {
-                this.categoryStore.isLoading = false;
-            }
+  // getCategory(refresh: Boolean = false) {
+  //     if (this.$state.categories && this.$state.categories.length > 0 && !refresh) {
+  //         return this.categories;
+  //     }
+
+  // }
+
+  async loadCategories(refresh: boolean = false): Promise<void> {
+    if (this.categoryStore.getCategory.length === 0 || refresh) {
+      this.categoryStore.isLoading = true;
+      this.categoryStore.error = null;
+      try {
+        const categories = await this.categoryRepository.getCategory();
+        this.categoryStore.setCategories(categories);
+      } catch (error) {
+        if (error instanceof AppException) {
+          this.categoryStore.error = `Failed to load categories: ${error.message}`;
+        } else {
+          this.categoryStore.error = 'An unexpected error occurred.';
         }
-
-
+        console.error(this.categoryStore.error, error);
+      } finally {
+        this.categoryStore.isLoading = false;
+      }
     }
+
+
+  }
 }
